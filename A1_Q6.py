16e544793caf5af5a3a15953f86cc2be85e22009
Joshua Lambert <joshualamb13@yahoo.com>
2019-11-05 21:54:40 -0400

    Cleanup

--- A1_Q6.py
+++ A1_Q6.py
@@ -5,8 +5,7 @@
 
 import random
 from A1_Q3 import findPrimitiveRoot
-from A1_Q6_DES import encryptDES
-from A1_Q6_DES import decryptDES
+from A1_Q6_DES import DESEncryption
 
 
 p = 5711
@@ -26,12 +25,27 @@ if __name__ == "__main__":
     d = generatePrivate(p)
     e = generatePublic(g, d, p)
 
-    privKey = encryptDES(str(d))
+    secrKey = b'secrpass'
+    desEncr = DESEncryption(secrKey)
 
+    pubKey = "{0},{1},{2}".format(p,g,e)
+    privKey = desEncr.encryptDES(str(d))
+
+    #write keys to files
     with open("privatekey.dat", "wb") as privKeyFile:
         privKeyFile.write(privKey)
 
-    with open("privatekey.dat", "rb") as readKeyFile:
-        val = readKeyFile.read()
-        decrVal = decryptDES(val)
-        print(decrVal.decode("utf8"))
+    with open("publickey.dat", "w") as pubKeyFile:
+        pubKeyFile.write(pubKey)
+
+
+
+    #load message
+    trumpMsgData = ""
+    with open("Trump.dat", "r") as trumpFile:
+        trumpMsgData = trumpFile.read()
+
+    # with open("privatekey.dat", "rb") as readKeyFile:
+    #     val = readKeyFile.read()
+    #     decrVal = desEncr.decryptDES(val)
+    #     print(decrVal)
--- A1_Q6_DES.py
+++ A1_Q6_DES.py
@@ -1,19 +1,24 @@
 import Crypto
 from Crypto.Cipher import DES
 
-password = b'secrpass'
-desLib = DES.new(password, DES.MODE_ECB)
+class DESEncryption:
+    def __init__(self, key):
+        self.key = key
+        self.desLib = DES.new(self.key, DES.MODE_ECB)
 
-##--Padding--##
-def pad(item):
-    if(len(item) % 8 != 0):
-        item += '0' * (8 - (len(item) % 8))
-    return item
+    ##--Padding--##
+    def pad(self, item):
+        if(len(item) % 8 != 0):
+            item += ' ' * (8 - (len(item) % 8))
+        return item
 
-def encryptDES(plaintext):
-    paddedItem = pad(plaintext)
-    return desLib.encrypt((paddedItem.encode("utf8")))
+    def removePadding(self, item):
+        return item.rstrip(" ")
 
+    def encryptDES(self, plaintext):
+        paddedItem = self.pad(plaintext)
+        return self.desLib.encrypt((paddedItem.encode("utf8")))
 
-def decryptDES(cipherText):
-    return desLib.decrypt(cipherText)
+    def decryptDES(self, cipherText):
+        decrVal = self.desLib.decrypt(cipherText).decode("utf8")
+        return self.removePadding(decrVal)
